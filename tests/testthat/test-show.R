57fbd025c7c18b759033f77f38d72b7e860dfb60
Artem Klevtsov <a.a.klevtsov@gmail.com>
2019-07-12 08:28:08 +0700

    Skip test show_regex if htmlwidgets is not installed

--- tests/testthat/test-show.R
+++ tests/testthat/test-show.R
@@ -1,6 +1,7 @@
 context("test show regex")
 
 test_that("test show_regex",{
+    skip_if_not_installed("htmlwidgets")
     show_regex(re2("(?P<testname>>this)( is)(?P<testname>>this)"))
     show_regex("show_regex")
     expect_error(show_regex(list()))
